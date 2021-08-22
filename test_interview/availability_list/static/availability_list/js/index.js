f314334653cabe280eedb98d48fb8c7e27f01e5a
omer perez <omerperez24@gmail.com>
2021-08-22 11:25:52 -0400

    test-interview

--- test_interview/availability_list/static/availability_list/js/index.js
+++ test_interview/availability_list/static/availability_list/js/index.js
@@ -2,6 +2,8 @@
 document.addEventListener("DOMContentLoaded", function(){
   
   let availability = 8;
+  let schedule = []
+  let selected_schedule = document.getElementById("selected_schedule");
   let ability = document.getElementById("availability");
   let divs = document.getElementsByTagName("div");
   let rectangles = document.getElementsByClassName("rectangle");
@@ -11,11 +13,13 @@ document.addEventListener("DOMContentLoaded", function(){
       div.classList.remove("available");
       availability += 1;
      //ciclo que busca todos los no disponible y los remueve
-      for(div of divs){
+      for(let div of divs){
           if (div.classList.contains("not_available")){
             removeClassNotAvailable(div); 
           }
-      }        
+      }
+      schedule = schedule.filter(item => item !== div.innerText)
+      selected_schedule.textContent = schedule.join(' <--> ')
       ability.textContent = availability;
   }
 
@@ -26,7 +30,9 @@ document.addEventListener("DOMContentLoaded", function(){
 
   const addClass = function(div){
     availability -= 1;
-    if (availability >= 0){ 
+    if (availability >= 0){
+          schedule.push(div.innerText)
+          selected_schedule.textContent = schedule.join(' <-> ')
           ability.textContent = availability;
           div.classList.add("available");
       }
@@ -35,11 +41,10 @@ document.addEventListener("DOMContentLoaded", function(){
   }
 
   const main = function(){
-    for (rectangle of rectangles) {
+    for (let rectangle of rectangles) {
       rectangle.addEventListener('click', (function() {
-    
+
       let div = document.getElementById(this.id);
-      
       if (div.classList.contains('available')){
          removeClassAvailable(div);
       }
@@ -58,10 +63,12 @@ document.addEventListener("DOMContentLoaded", function(){
 
   clear.addEventListener('click', (function() {
     availability = 8;
-    for(div of divs){
+    schedule = [];
+    for(let div of divs){
       div.classList.remove("not_available");
       div.classList.remove("available");
-    }        
+    }
+    selected_schedule.textContent = schedule;
     ability.textContent = availability;
   }));
 
